3425a3058aaaf6739b653036975b48bafc710c77
jackoubridge <jaoubridge@gmail.com>
2023-12-30 08:19:19 +0000

    Added ads section

--- react-app/src/components/Minecraft.js
+++ react-app/src/components/Minecraft.js
@@ -15,27 +15,28 @@ const Minecraft = React.forwardRef((props, ref) => {
 
     return (
         <>
-        <div className="container-fluid p-4 m-0 minecraft" ref={ref}>
-            <Header title={!props.ip ? "{serverIp}" : props.ip}/>
-            <div className="row w-100 m-0 d-flex justify-content-center text-light">
+            <div className="container-fluid p-4 m-0 minecraft" ref={ref}>
+                <Header title={!props.ip ? "{serverIp}" : props.ip} />
+                <div className="row w-100 mc-row m-0 d-flex justify-content-center text-light">
 
-                <div className="col col-lg-5 col-md-12 mc-col">
-                    <div className="mc-card rounded w-100 h-100 p-4 m-0 d-flex">
-                        <div className="mc-card-info my-auto w-100">
-                            <span className="w-100 d-block text-center rounded mb-1"><b>Server Status:</b>&nbsp;<span className="p-2 rounded" style={props.online === true ? online : offline}>{props.online === true ? "Online" : "Offline"}</span></span>
-                            <span className="w-100 d-block text-center"><b>Current players: </b> &nbsp; {props.playersCurrent}</span>
+                    <div className="col-6 mc-col">
+                        <div className="mc-card rounded w-100 h-100 p-4 m-0 d-flex">
+                            <div className="mc-card-info my-auto w-100">
+                                <span className="w-100 d-block text-center rounded mb-1"><b>Server Status:</b>&nbsp;<span className="p-2 rounded" style={props.online === true ? online : offline}>{props.online === true ? "Online" : "Offline"}</span></span>
+                                <span className="w-100 d-block text-center"><b>Current players: </b> &nbsp; {props.playersCurrent}</span>
+                            </div>
+
                         </div>
-                        
                     </div>
-                </div>
+                    <div className="col-6 m-0 p-0 ads"></div>
 
-                {/* <div className="col-12 col-lg-5 col-md-12 mc-col d-flex">
+                    {/* <div className="col-12 col-lg-5 col-md-12 mc-col d-flex">
                     <div className="mc-card rounded w-100 h-100 p-4 m-0">
                     </div>
                 </div> */}
-            
+
+                </div>
             </div>
-        </div>
         </>
     )
 });
