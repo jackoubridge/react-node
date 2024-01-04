c09bd22caba7e0aaf3dda4b7a950ef43c97b5331
jackoubridge <jaoubridge@gmail.com>
2024-01-04 21:43:40 +0000

    Updates

--- react-app/src/components/Minecraft.js
+++ react-app/src/components/Minecraft.js
@@ -22,10 +22,10 @@ const Minecraft = React.forwardRef((props, ref) => {
                     <div className="mc-col p-3">
                         <div className="mc-card rounded w-100 h-100 p-4 m-0 d-flex">
                             <div className="mc-card-info my-auto w-100 p-2">
-                                <span className="w-100 d-block mb-1"><b>Server Address: </b>&nbsp;{!props.ip ? "{serverIp}" : props.ip}</span>
-                                <span className="w-100 d-block mb-1"><b>Server Status:</b>&nbsp;<span className="status rounded" style={props.online === true ? online : offline}>{props.online === true ? "Online" : "Offline"}</span></span>
-                                <span className="w-100 d-block mb-1"><b>Current players:</b>&nbsp;{props.playersCurrent}</span>
-                                <span className="w-100 d-block"><i>Version {props.version}</i></span>
+                                <span className="w-100 d-block mb-1"><b>Server Address: </b>{!props.ip ? "{serverIp}" : props.ip}</span>
+                                <span className="w-100 d-block mb-1"><b>Server Status: </b><span className="status rounded" style={props.online === true ? online : offline}>{props.online === true ? "Online" : "Offline"}</span></span>
+                                <span className="w-100 d-block mb-1"><b>Currently Online: </b>{props.playersCurrent}</span>
+                                <span className="w-100 d-block"><b>Version: </b>{props.version}</span>
                                 <br></br>
                                 <span className="w-100 d-block">Please consider donating to support the server.</span>
                                 <a href="https://ko-fi.com/jiggzmc" target="_blank" type="button" class="btn btn-lg btn-primary">Donate</a>
